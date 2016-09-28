8e31dfb804af0911951ec9d8649d4eedbb2a5aee
Klara <klara.roehrl@gmail.com>
2016-09-28 10:57:49 +0200

    bootstrap methods are now tested. The bootstrap methods now also handle cases where bootstrap models return parameter vectors of different sizes and warn the user about such cases. Failed minimizations are also handled now and the user is warned about them.

--- tests/estimation/skill_model_test.py
+++ tests/estimation/skill_model_test.py
@@ -991,13 +991,13 @@ class TestBSMethods:
         assert_raises(
             AssertionError, smo._check_bs_samples, self)
 
-    def test__generate_bs_samples(self):
+    def test__select_bs_samples(self):
         np.random.seed(495)
         expected_samples = [
             ['id_1', 'id_1', 'id_1'],
             ['id_0', 'id_2', 'id_2'],
             ['id_2', 'id_2', 'id_1']]
-        calc_samples = smo._generate_bs_samples(self)
+        calc_samples = smo._select_bs_samples(self)
         assert_equal(calc_samples, expected_samples)
 
     def test__generate_bootstrap_data(self):
