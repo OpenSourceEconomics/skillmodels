08e9d7ddbed92672c3f4cae66150b61a55c36798
Mariam Petrosyan <44179698+mpetrosian@users.noreply.github.com>
2019-02-20 12:57:14 +0100

    Update simulate_data.py

--- skillmodels/simulation/simulate_data.py
+++ skillmodels/simulation/simulate_data.py
@@ -57,8 +57,8 @@ def add_missings(data, meas_names, p, q):
            p_m = p/(1-serial_corr), where  serial_corr = (q-p) in general != 0, since p != q. This means that in average the share of 
            missing values will be larger than p. Thus, p and q should be set accordingly given the desired share of missing values.
         - I would still suggest to draw period_0 bernoulli with the marginal probaility. Having run the function in a loop with 100 
-          iteration, the average share of missing measurements in all measurements is very close to p/(1-(q-p)) 
-          (for six pairs of p and q the average percentage deviation is less than 2 %). 
+          iteration, for 100 pairs of p and q , the average share of missing measurements in all measurements is very close to 
+          p/(1-(q-p)) (the average percentage deviation is less than 2 %). 
           Sounds like a nice result and a simple formula for deciding on p and q.
     """
 
