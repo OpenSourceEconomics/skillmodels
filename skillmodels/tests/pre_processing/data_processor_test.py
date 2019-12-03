b610f3adfc6e749bd943d980d517bdab147203b3
mariam p <mpetrosyan@uni-bonn.de>
2019-12-03 15:13:01 +0100

    corrected error message

--- skillmodels/tests/pre_processing/data_processor_test.py
+++ skillmodels/tests/pre_processing/data_processor_test.py
@@ -163,7 +163,7 @@ class TestObsValidty:
 
     def test_obs_validity(self):
         res = DataFrame(columns=["period", "variable", "issue"])
-        res["period"] = [0.0, 0.0, 1.0,1.0,1.0,1.0] 
+        res["period"] = [0.0, 0.0, 1.0,1.0,1.0,1.0]
         res["variable"] = ["c1","m1","c2","c3","m2","m3"]
         res["issue"] = [
             "All values are missing",
