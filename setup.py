91a2316a17503ab5befb6c49421cd2845c7b752e
Janos Gabler <janos.gabler@gmail.com>
2020-10-09 17:08:49 +0200

    Remove jax from meta.yaml and raise error with installation guide when it is not installed.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name="skillmodels", version="0.1.0dev3", packages=find_packages())
+setup(name="skillmodels", version="0.1.0dev4", packages=find_packages())
