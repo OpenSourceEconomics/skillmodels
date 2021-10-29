12838790a93ab747d64bd4eb75894c75fc3616b1
Janos Gabler <janos.gabler@gmail.com>
2021-10-29 15:34:28 +0200

    Redo plotting.

--- skillmodels/tests/test_visualize_factor_distributions.py
+++ skillmodels/tests/test_visualize_factor_distributions.py
@@ -4,6 +4,7 @@ import pandas as pd
 import yaml
 
 from skillmodels.likelihood_function import get_maximization_inputs
+from skillmodels.simulate_data import simulate_dataset
 from skillmodels.visualize_factor_distributions import (
     plot_pairwise_factor_distributions,
 )
@@ -13,7 +14,7 @@ from skillmodels.visualize_factor_distributions import (
 TEST_DIR = Path(__file__).parent.resolve()
 
 
-def test_visualize_factor_distributions_runs():
+def test_visualize_factor_distributions_runs_with_filtered_states():
     with open(TEST_DIR / "model2.yaml") as y:
         model_dict = yaml.load(y, Loader=yaml.FullLoader)
 
@@ -29,5 +30,29 @@ def test_visualize_factor_distributions_runs():
     filtered_states = debug_data["filtered_states"]
 
     plot_pairwise_factor_distributions(
-        states=filtered_states, model_dict=model_dict, add_3d_plots=True, period=1
+        states=filtered_states, model_dict=model_dict, add_3d_plots=False, period=1
+    )
+
+
+def test_visualize_factor_distributions_runs_with_simulated_states():
+    with open(TEST_DIR / "model2.yaml") as y:
+        model_dict = yaml.load(y, Loader=yaml.FullLoader)
+
+    params = pd.read_csv(TEST_DIR / "regression_vault" / f"one_stage_anchoring.csv")
+    params = params.set_index(["category", "period", "name1", "name2"])
+
+    control_means = pd.Series([0], index=["x1"])
+    control_sds = pd.Series([1], index=["x1"])
+
+    _, data = simulate_dataset(
+        model_dict,
+        params,
+        n_obs=100,
+        control_means=control_means,
+        control_sds=control_sds,
+        policies=None,
+    )
+
+    plot_pairwise_factor_distributions(
+        states=data, model_dict=model_dict, add_3d_plots=False, period=1
     )
--- skillmodels/visualize_factor_distributions.py
+++ skillmodels/visualize_factor_distributions.py
@@ -1,24 +1,22 @@
-import itertools
-import math
-import re
-
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.stats as st
+import pandas as pd
+import scipy
 import seaborn as sns
 
-from skillmodels.process_debug_data import create_state_ranges
 from skillmodels.process_model import process_model
 
 
-def plot_pairwise_factor_distributions(
+def plot_factor_distributions(
     states,
     model_dict,
     period,
     combine_plots_in_grid=True,
     add_3d_plots=False,
-    state_ranges=None,
     n_points=50,
+    lower_kde_kws=None,
+    diag_kde_kws=None,
+    surface_kws=None,
 ):
     """Visualize pairwise_factor_distributions in certain period.
 
@@ -30,8 +28,9 @@ def plot_pairwise_factor_distributions(
             estimate the distribution of the latent factors.
         model_dict (dict): The model specification. See: :ref:'model_specs'
         period (int): The selected period of the filtered states that are plotted.
-        combine_plots_in_grid (boolen): decide whether to retrun a grid of plots
-            or return a dict of individual plots. Default True.
+        combine_plots_in_grid (boolen): decide whether to retrun a one figure
+            containing subplots for each factor pair or a dictionary of
+            individual plots. Default True.
         add_3d_plots (boolen):decide whether to add 3D plots in grid of plots
             or in the dict of individual plots. Default False.
         state_ranges (dict): The keys are the names of the latent factors.
@@ -39,292 +38,166 @@ def plot_pairwise_factor_distributions(
             The state_ranges are used to define the axis limits of the plots.
         n_points (int): Number of grid points per plot. For 3d plots this is per
             dimension. Default 50.
+        lower_kde_kws (dict): Keyword arguments for seaborn.kdeplot, used to generate
+            the plots in the lower triangle of the grid, i.e. the two dimensional
+            kdeplot for each factor pair.
+        diag_kde_kws (dict): Keyword arguments for seaborn.kdeplot, used to generate
+            the plots on the diagonal of the grid, i.e. the one dimensional
+            kdeplot for each factor.
+        surface_kws (dict): Keyword arguments for Axes.plot_surface, used to generate
+            the plots in the upper triangle of the grid, i.e. the surface plot of the
+            kernel density estimates for each factor pair.
 
     Returns:
         matplotlib.Figure: The grid plot or dict of individual plots
 
     """
+    if add_3d_plots and not isinstance(states, pd.DataFrame):
+        raise ValueError("3d plots are only supported if states is a DataFrame")
+
+    lower_kde_kws = {} if lower_kde_kws is None else lower_kde_kws
+    diag_kde_kws = {} if diag_kde_kws is None else diag_kde_kws
+    surface_kws = {} if surface_kws is None else surface_kws
+
     model = process_model(model_dict)
-    if state_ranges is None:
-        if isinstance(states, list):
-            state_ranges = []
-            for state in states:
-                state_range = create_state_ranges(state[1], model["labels"]["factors"])
-                state_ranges.append(state_range)
-        elif isinstance(states, tuple):
-            state_range = create_state_ranges(states[1], model["labels"]["factors"])
-            state_ranges = [state_range]
-        else:
-            state_range = create_state_ranges(states, model["labels"]["factors"])
-            state_ranges = [state_range]
-    if isinstance(states, list):
-        pass
-    else:
-        states = [states]
-    plot_dict = {}
+    factors = model["labels"]["factors"]
+
+    data, hue = _process_data(states, period, factors)
+
+    grid = _get_axes_grid(
+        factors=factors,
+        combine_into_grid=combine_plots_in_grid,
+        add_3d_plots=add_3d_plots,
+    )
+    for row, fac1 in enumerate(factors):
+        for col, fac2 in enumerate(factors):
+            ax = grid[row][col]
+
+            if col < row:
+                kwargs = {
+                    "gridsize": n_points,
+                    **lower_kde_kws,
+                    "y": fac1,
+                    "x": fac2,
+                    "data": data,
+                    "hue": hue,
+                    "ax": ax,
+                }
+                _ = sns.kdeplot(**kwargs)
+            elif col == row:
+                kwargs = {
+                    "gridsize": n_points,
+                    **diag_kde_kws,
+                    "y": None,
+                    "x": fac1,
+                    "data": data,
+                    "hue": hue,
+                    "ax": ax,
+                }
+                _ = sns.kdeplot(**kwargs)
+            elif add_3d_plots:
+                _ = _3d_kdeplot(
+                    x=fac1,
+                    y=fac2,
+                    data=data,
+                    n_points=n_points,
+                    ax=ax,
+                    surface_kws=surface_kws,
+                )
+
+    sns.despine()
+
     if combine_plots_in_grid:
-        if add_3d_plots:
-            plot_dict = _plot_grid_2d_3d(states, state_ranges, period, n_points)
-        else:
-            plot_dict = _plot_changes_grid_2d(states, state_ranges, period, n_points)
+        out = grid[0][0].get_figure()
     else:
-        if add_3d_plots:
-            dict2 = _plot_changes_seperately_2d(states, state_ranges, period, n_points)
-            dict1 = _plot_3d_seperately(states, state_ranges, period)
-            plot_dict = {**dict1, **dict2}
-        else:
-            plot_dict = _plot_changes_seperately_2d(
-                states, state_ranges, period, n_points
-            )
-    return plot_dict
+        out = {}
+        for row, fac1 in enumerate(factors):
+            for col, fac2 in enumerate(factors):
+                _, visible = _get_ax_properties(row, col, add_3d_plots)
+                if visible:
+                    out[(fac1, fac2)] = grid[row][col].get_figure()
+
+    return out
 
 
-def _prepare_for_filtered_data(states, period):
-    data_period = states.query(f"period == {period}")
-    data_cleaned = data_period.drop(columns=["mixture", "period", "id"])
-    names = data_cleaned.keys()
-    factors = list(itertools.product(names, repeat=2))
-    return factors, data_cleaned
+def _3d_kdeplot(x, y, data, n_points, ax, surface_kws):
+    xx, yy, f = _calculate_kde_for_3d(data_cleaned=data, a=x, b=y, n_points=n_points)
+    kwargs = {
+        "rstride": 1,
+        "cstride": 1,
+        "linewidth": 0,
+        "cmap": "coolwarm",
+        "edgecolor": "none",
+        **surface_kws,
+    }
+    _ = ax.plot_surface(xx, yy, f, **kwargs)
+
+    ax.w_xaxis.pane.fill = False
+    ax.w_yaxis.pane.fill = False
+    ax.w_zaxis.pane.fill = False
 
 
-def _prepare_for_simulated_data(state, period):
-    data = state[1]
-    data = data.reset_index()
-    data_period = data.query(f"period == {period}")
-    data_cleaned = data_period.drop(columns=["period", "id"])
-    names = data_cleaned.keys()
-    factors = list(itertools.product(names, repeat=2))
-    return factors, data_cleaned
+def _process_data(states, period, factors):
+    if isinstance(states, pd.DataFrame):
+        data = states.query(f"period == {period}")[factors]
+        hue = None
+    else:
+        if not isinstance(states, dict):
+            states = {i: df for i, df in enumerate(states)}
+        to_concat = []
+        for name, df in states.items():
+            df = df.query(f"period == {period}")[factors]
+            df["scenario"] = name
+            to_concat.append(df)
+        data = pd.concat(to_concat)
+        hue = "scenario"
+    return data, hue
 
 
-def prepare_axes_for_2d_changes(ax, state_ranges, a, b, period):
-    lower_bounds = []
-    upper_bounds = []
-    for state_range in state_ranges:
-        stat_min = min(
-            state_range[a].loc[period]["minimum"], state_range[b].loc[period]["minimum"]
-        )
-        stat_max = max(
-            state_range[a].loc[period]["maximum"], state_range[b].loc[period]["maximum"]
-        )
-        lower_bounds.append(stat_min)
-        upper_bounds.append(stat_max)
-    lower_bound = math.floor(min(lower_bounds)) - 1
-    upper_bound = math.ceil(max(upper_bounds)) + 1
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.set(xlim=(lower_bound, upper_bound), ylim=(lower_bound, upper_bound))
-    return ax
+def _get_axes_grid(factors, combine_into_grid, add_3d_plots):
+    dim = len(factors)
+    axes = []
+    if combine_into_grid:
+        fig = plt.figure(figsize=(dim * 5, dim * 5))
+        gs = fig.add_gridspec(dim, dim)
+        for row in range(len(factors)):
+            grid_row = []
+            for col in range(len(factors)):
+                proj, visible = _get_ax_properties(row, col, add_3d_plots)
+                ax = fig.add_subplot(gs[row, col], projection=proj)
+                ax.set_visible(visible)
+                grid_row.append(ax)
+            axes.append(grid_row)
+    else:
+        for row in range(len(factors)):
+            grid_row = []
+            for col in range(len(factors)):
+                proj, visible = _get_ax_properties(row, col, add_3d_plots)
+                fig, ax = plt.subplots(figsize=(5, 5), subplot_kw={"projection": proj})
+                grid_row.append(ax)
+                ax.set_visible(visible)
+            axes.append(grid_row)
+
+    return axes
 
 
-def _plot_changes_grid_2d(states, state_ranges, period, n_points):
-    factors = []
-    datas = []
-    for state in states:
-        if isinstance(state, tuple):
-            factor, data = _prepare_for_simulated_data(state, period)
-        else:
-            factor, data = _prepare_for_filtered_data(state, period)
-        factors.append(factor)
-        datas.append(data)
-
-    fig = plt.figure(figsize=(15, 15))
-    fig.suptitle(f"Grid of plots for Period {period}")
-    size = int(np.sqrt(len(factors[0])))
-    gs = fig.add_gridspec(size, size)
-    for a, b in factors[0]:
-        row = int(re.sub("[^0-9]", "", a))
-        col = int(re.sub("[^0-9]", "", b))
-        if row > col:
-            ax = fig.add_subplot(gs[row - 1, col - 1])
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            for data in datas:
-                sns.kdeplot(
-                    data=data,
-                    x=data[a],
-                    y=data[b],
-                    gridsize=n_points,
-                    ax=ax,
-                    fill=True,
-                    alpha=0.7,
-                )
-
-        elif row == col:
-            ax = fig.add_subplot(gs[row - 1, col - 1])
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            for data in datas:
-                sns.kdeplot(data=data, x=data[a], gridsize=n_points, ax=ax)
-            ax.set(ylim=(0, None))
-    return fig
+def _get_ax_properties(row, col, add_3d):
+    projection = "3d" if add_3d and col > row else None
+    visible = col <= row or add_3d
+    return projection, visible
 
 
-def _plot_changes_seperately_2d(states, state_ranges, period, n_points):
-    figure_dict = {}
-    factors = []
-    datas = []
-    for state in states:
-        if isinstance(state, tuple):
-            factor, data = _prepare_for_simulated_data(state, period)
-        else:
-            factor, data = _prepare_for_filtered_data(state, period)
-        factors.append(factor)
-        datas.append(data)
-    for a, b in factors[0]:
-        row = int(re.sub("[^0-9]", "", a))
-        col = int(re.sub("[^0-9]", "", b))
-        if row > col:
-            fig, ax = plt.subplots()
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            for data in datas:
-                sns.kdeplot(
-                    data=data,
-                    x=data[a],
-                    y=data[b],
-                    gridsize=n_points,
-                    ax=ax,
-                    fill=True,
-                    alpha=0.7,
-                )
-            fig.suptitle(f"{a}_{b}_2D_Period {period}")
-            figure_dict[f"{a}_{b}_2D_Period {period}"] = fig
-
-        elif row == col:
-            fig, ax = plt.subplots()
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            for data in datas:
-                sns.kdeplot(data=data, x=data[a], gridsize=n_points, ax=ax)
-                fig.suptitle(f"{a}_2D_Period {period}")
-                figure_dict[f"{a}_2D_Period {period}"] = fig
-            ax.set(ylim=(0, None))
-    return figure_dict
-
-
-def _calculate_kde_for_3d(data_cleaned, a, b, lower_bound, upper_bound):
-
+def _calculate_kde_for_3d(data_cleaned, a, b, n_points):
     x = data_cleaned[a]
     y = data_cleaned[b]
-    xx, yy = np.mgrid[lower_bound:upper_bound:50j, lower_bound:upper_bound:50j]
+    variables = [a, b]
+    lb1, lb2 = data_cleaned[variables].min()
+    ub1, ub2 = data_cleaned[variables].max()
+
+    cp = complex(n_points)
+    xx, yy = np.mgrid[lb1:ub1:cp, lb2:ub2:cp]
     positions = np.vstack([xx.ravel(), yy.ravel()])
     values = np.vstack([x, y])
-    kernel = st.gaussian_kde(values)
+    kernel = scipy.stats.gaussian_kde(values)
     f = np.reshape(kernel(positions).T, xx.shape)
     return xx, yy, f
-
-
-def _prepare_axes_for_3d(ax, state_ranges, a, b, period):
-    lower_bounds = []
-    upper_bounds = []
-    for state_range in state_ranges:
-        stat_min = min(
-            state_range[a].loc[period]["minimum"], state_range[b].loc[period]["minimum"]
-        )
-        stat_max = max(
-            state_range[a].loc[period]["maximum"], state_range[b].loc[period]["maximum"]
-        )
-        lower_bounds.append(stat_min)
-        upper_bounds.append(stat_max)
-    lower_bound = math.floor(min(lower_bounds))
-    upper_bound = math.ceil(max(upper_bounds))
-
-    ax.set(
-        xlim=(lower_bound, upper_bound), ylim=(lower_bound, upper_bound), zlim=(0, None)
-    )
-    ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.set_zlabel("KDE")
-    ax.grid(False)
-    ax.view_init(30, 35)
-    return ax, lower_bound, upper_bound
-
-
-def _plot_grid_2d_3d(states, state_ranges, period, n_points):
-    if isinstance(states[0], tuple):
-        factors, data_cleaned = _prepare_for_simulated_data(states[0], period)
-    else:
-        factors, data_cleaned = _prepare_for_filtered_data(states[0], period)
-
-    fig = plt.figure(figsize=(15, 15))
-    fig.suptitle(f"Grid of plots for Period {period}")
-    size = int(np.sqrt(len(factors)))
-    gs = fig.add_gridspec(size, size)
-    for a, b in factors:
-        row = int(re.sub("[^0-9]", "", a))
-        col = int(re.sub("[^0-9]", "", b))
-        if row > col:
-            ax = fig.add_subplot(gs[row - 1, col - 1])
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            sns.kdeplot(
-                data=data_cleaned,
-                x=data_cleaned[a],
-                y=data_cleaned[b],
-                gridsize=n_points,
-                ax=ax,
-                fill=True,
-                alpha=0.7,
-            )
-
-        elif row == col:
-            ax = fig.add_subplot(gs[row - 1, col - 1])
-            ax = prepare_axes_for_2d_changes(ax, state_ranges, a, b, period)
-            sns.kdeplot(data=data_cleaned, x=data_cleaned[a], gridsize=n_points, ax=ax)
-            ax.set(ylim=(0, None))
-
-        else:
-            ax = fig.add_subplot(gs[row - 1, col - 1], projection="3d")
-            ax, lower_bound, upper_bound = _prepare_axes_for_3d(
-                ax, state_ranges, a, b, period
-            )
-            xx, yy, f = _calculate_kde_for_3d(
-                data_cleaned, a, b, lower_bound, upper_bound
-            )
-            surf = ax.plot_surface(
-                xx,
-                yy,
-                f,
-                rstride=1,
-                cstride=1,
-                linewidth=0,
-                antialiased=False,
-                cmap="coolwarm",
-                edgecolor="none",
-            )
-            fig.colorbar(surf, shrink=0.5, aspect=5)
-    return fig
-
-
-def _plot_3d_seperately(states, state_ranges, period):
-    if isinstance(states[0], tuple):
-        factors, data_cleaned = _prepare_for_simulated_data(states[0], period)
-    else:
-        factors, data_cleaned = _prepare_for_filtered_data(states[0], period)
-    fig = plt.figure(figsize=(15, 15))
-    figure_dict = {}
-    for a, b in factors:
-        row = int(re.sub("[^0-9]", "", a))
-        col = int(re.sub("[^0-9]", "", b))
-        if row > col:
-            fig = plt.figure()
-            ax = plt.axes(projection="3d")
-            fig.suptitle(f"{a}_{b}_3D_Period {period}")
-            ax, lower_bound, upper_bound = _prepare_axes_for_3d(
-                ax, state_ranges, a, b, period
-            )
-            xx, yy, f = _calculate_kde_for_3d(
-                data_cleaned, a, b, lower_bound, upper_bound
-            )
-            surf = ax.plot_surface(
-                xx,
-                yy,
-                f,
-                rstride=1,
-                cstride=1,
-                linewidth=0,
-                antialiased=False,
-                cmap="coolwarm",
-                edgecolor="none",
-            )
-            fig.colorbar(surf, shrink=0.5, aspect=5)
-            figure_dict[f"{a}_{b}_3D_Period {period}"] = fig
-    return figure_dict
