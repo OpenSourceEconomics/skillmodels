f268188868fccdce5daa97f18b310d3a31728210
Janos Gabler <janos.gabler@gmail.com>
2022-03-29 11:37:23 +0200

    Get visualize factor distribution with simulated states to run in example model.

--- skillmodels/tests/test_visualize_factor_distributions.py
+++ skillmodels/tests/test_visualize_factor_distributions.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import pandas as pd
-import pytest
 import yaml
 
 from skillmodels.likelihood_function import get_maximization_inputs
@@ -34,26 +33,21 @@ def test_visualize_factor_distributions_runs_with_filtered_states():
     )
 
 
-@pytest.mark.xfail(reason="Simulate is not yet updated to observed factors.")
 def test_visualize_factor_distributions_runs_with_simulated_states():
     with open(TEST_DIR / "model2.yaml") as y:
         model_dict = yaml.load(y, Loader=yaml.FullLoader)
 
+    data = pd.read_stata(TEST_DIR / "model2_simulated_data.dta")
+    data.set_index(["caseid", "period"], inplace=True)
+
     params = pd.read_csv(TEST_DIR / "regression_vault" / f"one_stage_anchoring.csv")
     params = params.set_index(["category", "period", "name1", "name2"])
 
-    control_means = pd.Series([0], index=["x1"])
-    control_sds = pd.Series([1], index=["x1"])
+    max_inputs = get_maximization_inputs(model_dict, data)
+    params = params.loc[max_inputs["params_template"].index]
 
-    _, data = simulate_dataset(
-        model_dict,
-        params,
-        n_obs=100,
-        control_means=control_means,
-        control_sds=control_sds,
-        policies=None,
-    )
+    _, latent_data = simulate_dataset(model_dict, params, data=data, policies=None)
 
     plot_factor_distributions(
-        states=data, model_dict=model_dict, add_3d_plots=False, period=1
+        states=latent_data, model_dict=model_dict, add_3d_plots=False, period=1
     )
