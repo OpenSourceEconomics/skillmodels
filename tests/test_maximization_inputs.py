ceff7aadba5bdc9d6481608b09fc21eabf1fd58f
Hans-Martin v. Gaudecker <hmgaudecker@gmail.com>
2024-09-24 18:33:39 +0200

    Add function to check whether the model has investments.

--- tests/test_maximization_inputs.py
+++ tests/test_maximization_inputs.py
@@ -1,7 +1,8 @@
 import jax.numpy as jnp
 import numpy as np
+import pytest
 
-from skillmodels.maximization_inputs import _to_numpy
+from skillmodels.maximization_inputs import _to_numpy, model_has_investments
 
 
 def test_to_numpy_with_dict():
@@ -19,3 +20,37 @@ def test_to_numpy_one_array():
 def test_to_numpy_one_float():
     calculated = _to_numpy(3.5)
     assert isinstance(calculated, float)
+
+
+def test_model_has_investments_not_specified():
+    factors = {"a": {}}
+    assert model_has_investments(factors) == False
+
+
+def test_model_has_investments_wrong_type():
+    factors = {"a": {"is_investment": 3}}
+    with pytest.raises(ValueError):
+        model_has_investments(factors)
+
+
+def test_model_has_investments_wrong_constellation():
+    factors = {"a": {"is_investment": False, "is_correction": True}}
+    with pytest.raises(ValueError):
+        model_has_investments(factors)
+
+
+def test_model_has_investments_indeed():
+    factors = {
+        "a": {"is_investment": True, "is_correction": False},
+        "b": {"is_investment": False, "is_correction": False},
+    }
+    assert model_has_investments(factors) == True
+
+
+def test_model_has_investments_and_correction():
+    factors = {
+        "a": {"is_investment": True, "is_correction": False},
+        "b": {"is_investment": False, "is_correction": False},
+        "c": {"is_investment": True, "is_correction": True},
+    }
+    assert model_has_investments(factors) == True
